433cfe47fa1f642a507f916e579773ef91070297
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-22 09:59:12 -0400

    fix: Sweetalert when login fails

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,19 +1,21 @@
 import { types } from '../types/types';
 import { fetchWithoutToken } from '../helpers/fetch';
-
+import Swal from 'sweetalert2';
 
 export const startLogin = (email, password) => {
   return async (dispatch) => {
 
     const resp = await fetchWithoutToken('auth', { email, password }, 'POST');
-    const { ok, name, token, uid } = await resp.json(); // {name,ok,token,uid}
+    const body = await resp.json(); // {name,ok,token,uid}
     
-    if (ok) {
-      localStorage.setItem('token', token);
+    if (body.ok) {
+      localStorage.setItem('token', body.token);
       localStorage.setItem('token-init-date', new Date().getTime()); // to know when token was created
 
-      dispatch(login({ name, uid }));
+      dispatch(login({ name: body.name, uid: body.uid }));
 
+    } else {
+      Swal.fire('Error', body.msg, 'error');
     }
 
   }
