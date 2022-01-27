cf26a4e05c0216dac7fa9b52639694722384da8f
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-27 19:42:25 -0400

    test: test on CalendarScreen
    events from <Calendar /> like messages, onDoubleClickEvent, onSelectEvent, onView

--- src/__tests__/components/calendar/CalendarScreen.test.js
+++ src/__tests__/components/calendar/CalendarScreen.test.js
@@ -7,6 +7,17 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { CalendarScreen } from '../../../components/calendar/CalendarScreen';
 
+import { messages } from '../../../helpers/calendar-messages-es';
+import { types } from '../../../types/types';
+import { eventSetActive } from '../../../actions/eventsActions';
+import { act } from '@testing-library/react';
+
+jest.mock('../../../actions/eventsActions', () => ({
+  eventSetActive: jest.fn(),
+  eventStartLoading: jest.fn()
+}));
+
+Storage.prototype.setItem = jest.fn()
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -25,7 +36,7 @@ const initState = {
   }
 };
 const store = mockStore(initState);
-// store.dispatch = jest.fn();
+store.dispatch = jest.fn();
 
 const wrapper = mount(
   <Provider store={ store }>
@@ -40,8 +51,26 @@ describe('Test on <CalendarScreen >', () => {
   });
 
   test('should interact with calendar', () => {
+    const calendar = wrapper.find('Calendar');
     
+    // check messages
+    const calendarMessages = calendar.prop('messages');
+    expect(calendarMessages).toEqual(messages);
+
+    // double click
+    calendar.prop('onDoubleClickEvent')();
+    // the action is synchronous
+    expect(store.dispatch).toHaveBeenCalledWith({ type: types.uiOpenModal });
+    
+    //onSelectEvent
+    calendar.prop('onSelectEvent')({ start: 'Hola'}); // mock eventSetActive
+    expect(eventSetActive).toHaveBeenCalledWith({ start: 'Hola'});
+    
+    // onView check if saves to localStorage
+    act(() => {
+      // use act because it modifies the state
+      calendar.prop('onView')('week');
+      expect(localStorage.setItem).toHaveBeenCalledWith('lastView', 'week');
+    });
   });
-  
-  
 });
