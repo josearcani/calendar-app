bb2838874a12eb156d90f062d875baa267f2b4b2
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-27 15:57:08 -0400

    test: test AppRouter public and private routes

--- src/__tests__/routers/AppRouter.test.js
+++ src/__tests__/routers/AppRouter.test.js
@@ -6,15 +6,17 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { AppRouter } from '../../Routers/AppRouter';
-
+import { startChecking } from '../../actions/authActions';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-
-
+jest.mock('../../actions/authActions', () => ({
+  startChecking: jest.fn()
+}))
 
 describe('Test on <AppRouter />', () => {
+
   test('should show loader', () => {
     const initState = {
       auth: {
@@ -22,6 +24,7 @@ describe('Test on <AppRouter />', () => {
       }
     };
     const store = mockStore(initState);
+    store.dispatch = jest.fn();
     const wrapper = mount(
       <Provider store={ store }>
         <AppRouter />
@@ -31,5 +34,56 @@ describe('Test on <AppRouter />', () => {
     expect(wrapper).toMatchSnapshot();
     expect( wrapper.find('h1').text().trim()).toBe('Espere');
   });
+
+  test('should show public route', () => {
+    const initState = {
+      auth: {
+        checking: false,
+        name: null,
+        uid: null
+      }
+    };
+    const store = mockStore(initState);
+    store.dispatch = jest.fn();
+    const wrapper = mount(
+      <Provider store={ store }>
+        <AppRouter />
+      </Provider>
+    );
+
+    // const actions = store.getActions();
+    // console.log(actions);
+      expect(startChecking).toHaveBeenCalledTimes(1);
+      expect(store.dispatch).toHaveBeenCalledTimes(1);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.login-container').exists()).toBe(true);
+  });
   
+  test('should show private route', () => {
+    const initState = {
+      auth: {
+        checking: false,
+        name: 'joseph',
+        uid: '231ion31inoianfa34adva'
+      },
+      calendar: {
+        events: [],
+        activeEvent: {}
+      },
+      ui: {
+        modalOpen: false
+      }
+    };
+    const store = mockStore(initState);
+    store.dispatch = jest.fn();
+
+    const wrapper = mount(
+      <Provider store={ store }>
+        <AppRouter />
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.calendar-screen').exists()).toBe(true);
+  });
 });
