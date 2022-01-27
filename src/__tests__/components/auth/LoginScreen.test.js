13dfedc18034d199cbcd95a0899fbfbf3800cdf7
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-27 16:38:57 -0400

    test: test LoginScreen register form

--- src/__tests__/components/auth/LoginScreen.test.js
+++ src/__tests__/components/auth/LoginScreen.test.js
@@ -6,11 +6,17 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { LoginScreen } from '../../../components/auth/LoginScreen';
-import { startLogin } from '../../../actions/authActions';
+import { startLogin, startRegister } from '../../../actions/authActions';
+import Swal from 'sweetalert2';
+
+jest.mock('sweetalert2', () => ({
+  fire: jest.fn()
+}));
 
 jest.mock('../../../actions/authActions', () => ({
-  startLogin: jest.fn()
-}))
+  startLogin: jest.fn(),
+  startRegister: jest.fn()
+}));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -26,6 +32,11 @@ const wrapper = mount(
 )
 
 describe('Test on <LoginScreen />', () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
+
   test('should render normally', () => {
     expect(wrapper).toMatchSnapshot();
   });
@@ -48,6 +59,33 @@ describe('Test on <LoginScreen />', () => {
     expect(startLogin).toHaveBeenCalledWith('john@email.com', 'secret');
   });
 
+  test('should FAIL register when passwords do not match', () => {
+    wrapper.find('input[name="rPassword2"]').simulate('change', {
+      target: {
+        name: 'rPassword2',
+        value: '123456'
+      }
+    })
+    wrapper.find('form').at(1).simulate('submit', { preventDefault(){} });
+    expect(startRegister).not.toHaveBeenCalled();
+    expect(Swal.fire).toHaveBeenCalledWith('Error', 'Las contraseñas deben de ser iguales', 'error');
+  });
   
-  
+  test('should register normally', () => {
+    wrapper.find('input[name="rPassword"]').simulate('change', {
+      target: {
+        name: 'rPassword',
+        value: '123456'
+      }
+    })
+    wrapper.find('input[name="rPassword2"]').simulate('change', {
+      target: {
+        name: 'rPassword2',
+        value: '123456'
+      }
+    })
+    wrapper.find('form').at(1).simulate('submit', { preventDefault(){} });
+    expect(startRegister).toHaveBeenCalledWith('nataly', 'nataly@test.com', '123456');
+    expect(Swal.fire).not.toHaveBeenCalled();
+  });
 });
